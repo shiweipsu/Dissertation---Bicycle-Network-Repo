49b2a3fcde305d862f3e19b24d4caac0f35ed03a
shiweipsu <shi.wei8805@gmail.com>
2018-11-29 10:23:37 -0500

    init commit

---
+++ src/acs_data.R
@@ -0,0 +1,51 @@
+library(readxl)
+library(stringr)
+library(tidyverse)
+library(ggplot2)
+
+acs_bike_big <- read_excel("ACS/supplemental-table3 (1).xlsx", skip = 5) %>% mutate(type="Big")
+acs_bike_median <- read_excel("ACS/supplemental-table2 (1).xlsx", skip = 5) %>% mutate(type="Mid")
+acs_bike_small <- read_excel("ACS/supplemental-table1 (1).xlsx", skip = 5) %>% mutate(type="Small")
+
+acs_bike <- rbind(acs_bike_big, acs_bike_median, acs_bike_small)
+colnames(acs_bike) <- c("city", "bicycle_number", "bicycle_perc","margin_error","type")
+acs_bike$bicycle_perc <- as.numeric(acs_bike$bicycle_perc)
+acs_bike <- acs_bike %>% 
+  separate(city, c("city", "state"), sep=", ")
+  
+  
+summary(filter(acs_bike, state=="California")$bicycle_perc)
+hist(filter(acs_bike, state=="California")$bicycle_perc)
+
+acs_bike_30 <- acs_bike %>% 
+  arrange(desc(bicycle_perc)) %>% slice(1:30) %>% 
+  mutate(City = paste(city, state, sep=", "))
+
+acs_bike_big <- acs_bike %>% filter(type=="Big") %>% 
+  arrange(desc(bicycle_perc)) %>% slice(1:20) %>% 
+  mutate(City = paste(city, state, sep=", "))
+
+acs_bike_mid <-  acs_bike %>% filter(type=="Mid") %>% 
+  arrange(desc(bicycle_perc)) %>% slice(1:20) %>% 
+  mutate(City = paste(city, state, sep=", "))
+
+acs_bike_sm <- acs_bike %>% filter(type=="Small") %>% 
+  arrange(desc(bicycle_perc)) %>% slice(1:20) %>% 
+  mutate(City = paste(city, state, sep=", "))
+
+acs
+
+write.csv(acs_bike_30, "ACS/acs_bike_30.csv")
+
+acs_bike_ca <- read.csv("ACS/ACS_16_5YR_S0801_CA/ACS_16_5YR_S0801_with_ann.csv", skip = 1) %>% 
+  select(1,3, 64)
+colnames(acs_bike_ca) <- c("GEOID","Census_Tract", "bike")
+acs_bike_alameda <- acs_bike_ca %>% slice(1:154) %>% mutate(bike = as.numeric(as.character(bike)))
+hist(acs_bike_alameda$bike)
+
+acs_bike_or <- read.csv("ACS/ACS_16_5YR_S0801_OR/ACS_16_5YR_S0801_with_ann.csv", skip = 1) %>% 
+  select(1,3, 64)
+colnames(acs_bike_or) <- c("GEOID","Census_Tract", "bike")
+acs_bike_or$bike <- as.numeric(as.character(acs_bike_or$bike))
+hist(acs_bike_or$bike)
+write.csv(acs_bike_or, "ACS/acs_bike_multnomah.csv")
---
+++ src/data explore.R
@@ -0,0 +1,93 @@
+setwd("G:/My Drive/Dissertation/Data")
+library(dplyr)
+
+# read hh data
+hh <- read.csv("NHTS/2017/hhpub.csv")
+person <- read.csv("NHTS/2017/perpub.csv")
+trip <- read.csv("NHTS/2017/trippub.csv")
+
+trip_hh <- left_join(trip, hh, by="HOUSEID")
+
+# add-on state: AZ, CA, GA, IA, MD, NC, NY, SC, TX, WI
+
+# divided by state
+ny <- trip %>% filter(HHSTATE=="NY")
+ca <- trip %>% filter(HHSTATE=="CA")
+tx <- trip %>% filter(HHSTATE=="TX")
+or <- trip %>% filter(HHSTATE=="OR")
+
+table(ny$TRPTRANS)
+nyt <- xtabs(~TRPTRANS+TRIPPURP, data=ny)
+round(prop.table(nyt, 2),3)
+
+table(ca$TRPTRANS)
+cat <- xtabs(~TRPTRANS+TRIPPURP, data=ca)
+round(prop.table(cat, 2),3)
+
+table(tx$TRPTRANS)
+txt <- xtabs(~TRPTRANS+TRIPPURP, data=tx)
+round(prop.table(txt, 2),3)
+
+table(or$TRPTRANS)
+ort <- xtabs(~TRPTRANS+TRIPPURP, data=or)
+round(prop.table(ort, 2),3)
+
+# divided by CBSA
+sf <- trip %>% filter(HH_CBSA==41860)
+table(sf$TRPTRANS)
+sft <- xtabs(~TRPTRANS+TRIPPURP, data=sf)
+round(prop.table(sft, 2),3)
+
+sd <- trip %>% filter(HH_CBSA==41740)
+table(sd$TRPTRANS)
+sdt <- xtabs(~TRPTRANS+TRIPPURP, data=sd)
+round(prop.table(sdt, 2),3)
+
+dalas <-trip %>% filter(HH_CBSA==19100)
+table(dalas$TRPTRANS)
+dalast <- xtabs(~TRPTRANS+TRIPPURP, data=dalas)
+round(prop.table(dalast, 2),3)
+
+ny <- trip %>% filter(HH_CBSA==35620)
+table(ny$TRPTRANS)
+nyt <- xtabs(~TRPTRANS+TRIPPURP, data=ny)
+round(prop.table(nyt, 2),3)
+
+pdx <- trip %>% filter(HH_CBSA==38900)
+table(pdx$TRPTRANS)
+pdxt <- xtabs(~TRPTRANS+TRIPPURP, data=pdx)
+round(prop.table(pdxt, 2),3)
+
+
+
+
+# 2009 --------------------------------------------------------------------
+
+# read hh data
+hh <- read.csv("NHTS/2009/HHV2PUB.CSV")
+person <- read.csv("NHTS/2009/PERV2PUB.CSV")
+trip <- read.csv("NHTS/2009/DAYV2PUB.CSV")
+
+table(trip$HHSTATE)
+# add-on states: CA, FL, NC, NY, SC, TX
+
+ny <- trip %>% filter(HHSTATE=="NY")
+ca <- trip %>% filter(HHSTATE=="CA")
+tx <- trip %>% filter(HHSTATE=="TX")
+or <- trip %>% filter(HHSTATE=="OR")
+
+table(ny$TRPTRANS)
+nyt <- xtabs(~TRPTRANS+TRIPPURP, data=ny)
+round(prop.table(nyt, 2),3)
+
+table(ca$TRPTRANS)
+cat <- xtabs(~TRPTRANS+TRIPPURP, data=ca)
+round(prop.table(cat, 2),3)
+
+table(tx$TRPTRANS)
+txt <- xtabs(~TRPTRANS+TRIPPURP, data=tx)
+round(prop.table(txt, 2),3)
+
+table(or$TRPTRANS)
+ort <- xtabs(~TRPTRANS+TRIPPURP, data=or)
+round(prop.table(ort, 2),3)
